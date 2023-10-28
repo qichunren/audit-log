ae5fbaef37cf3c0cd1c0a8e13283d68a32d285f3
qichunren <whyruby@gmail.com>
2023-10-28 16:52:52 +0800

    Support use audit! helper method in ActionCable channel class.

--- lib/audit-log/controller_helper.rb
+++ lib/audit-log/controller_helper.rb
@@ -5,6 +5,8 @@ module AuditLog
     # audit!(:edit_account, @account, payload: account_params)
     def audit!(action, record = nil, payload: nil, user: nil)
       user ||= self.send(AuditLog.config.current_user_method.to_sym)
+      # audit! method may used in no controller class, eg: ActionCable channel
+      request = nil unless defined?(request)
       AuditLog.audit!(action, record, payload: payload, request: request, user: user)
     end
   end
