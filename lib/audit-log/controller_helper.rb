49830b66c3707ee4ab11d9ca031459c903ddb14f
qichunren <whyruby@gmail.com>
2025-02-18 16:18:22 +0800

    Fixed request nil bug

--- lib/audit-log/controller_helper.rb
+++ lib/audit-log/controller_helper.rb
@@ -6,8 +6,8 @@ module AuditLog
     def audit!(action, record = nil, payload: nil, user: nil)
       user ||= self.send(AuditLog.config.current_user_method.to_sym)
       # audit! method may used in no controller class, eg: ActionCable channel
-      request = nil unless defined?(request)
-      AuditLog.audit!(action, record, payload: payload, request: request, user: user)
+      current_request = respond_to?(:request) ? request : nil
+      AuditLog.audit!(action, record, payload: payload, request: current_request, user: user)
     end
   end
 end
